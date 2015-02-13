98fa15d8c104cec64d058eb31d6a6ec4732ab26a
Kevin Giguère <kgiguere@o2web.ca>
2015-02-13 17:07:21 -0500

    test update event for ClosingPromp

--- closing_promp.py
+++ closing_promp.py
@@ -15,12 +15,16 @@ class ClosingPromp():
 		p2 = self.end+len(self.codewave.brakets)*3+len(self.codewave.closeChar)+2
 		self.codewave.editor.setCursorPos(p2)
 		Npp.editor.addSelection(p1,p1)
-		Npp.editor.callback(self.onAnyChange, [Npp.SCINTILLANOTIFICATION.CHARADDED])
-	def onAnyChange(self,ch):
-		logger.log('onAnyChange')
+		Npp.editor.callback(self.onAddChar, [Npp.SCINTILLANOTIFICATION.CHARADDED])
+		return self
+	def onAddChar(self,ch):
+		# logger.log('added :'+str(ch))
+		if ch['ch'] ==  32:
+			logger.log('space added')
 	def stop(self):
 		if self.codewave.closingPromp == this :
 			self.codewave.closingPromp = null
+		Npp.editor.clearCallbacks(self.onAddChar)
 	def cancel(self):
 		openBounds = self.whithinOpenBounds(self.start+len(self.codewave.brakets))
 		if openBounds is not None :
--- codewave_activate.py
+++ codewave_activate.py
@@ -5,10 +5,18 @@ import codewave_npp.npp_editor
 reload(codewave_npp.npp_editor)
 import codewave_npp.cmd_instance
 reload(codewave_npp.cmd_instance)
+import codewave_npp.logger
+reload(codewave_npp.logger)
 
 
 from codewave_npp.codewave import Codewave
 from codewave_npp.npp_editor import NppEditor
 
-cw = Codewave(NppEditor())
+
+if 'cw' not in vars() :
+	codewave_npp.logger.log('init codewave');
+	cw = Codewave(NppEditor())
+	
 cw.onActivationKey()
+
+# codewave_npp.logger.log('expand: '+npp_emmet.ctx.js().locals.emmet.expandAbbreviation('ul>li*5>a'))
