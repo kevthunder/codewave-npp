b37515a1a1537737a05cde8511c7733b4b7cab2f
Kevin Giguère <kgiguere@o2web.ca>
2015-02-18 10:47:33 -0500

    moved core components

--- codewave_activate.py
+++ codewave_activate.py
@@ -1,16 +1,16 @@
 # reload
-import codewave_npp.codewave
-reload(codewave_npp.codewave)
+import codewave_npp.codewave.codewave
+reload(codewave_npp.codewave.codewave)
 import codewave_npp.npp_editor
 reload(codewave_npp.npp_editor)
-import codewave_npp.logger
-reload(codewave_npp.logger)
+import codewave_npp.codewave.logger
+reload(codewave_npp.codewave.logger)
 
 debug = True
 if debug or 'cw' not in vars() or cw is None :
-	codewave_npp.codewave.init()
-	codewave_npp.logger.log('init codewave');
-	cw = codewave_npp.codewave.Codewave(codewave_npp.npp_editor.NppEditor())
+	codewave_npp.codewave.codewave.init()
+	codewave_npp.codewave.logger.log('init codewave');
+	cw = codewave_npp.codewave.codewave.Codewave(codewave_npp.npp_editor.NppEditor())
 	
 cw.onActivationKey()
 
