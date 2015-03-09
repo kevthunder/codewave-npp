9174b0ab00c07d687df7d2851667f7e70e9bbb6e
Kevin Giguère <kgiguere@o2web.ca>
2015-03-09 11:30:10 -0400

    reloader

--- codewave_activate.py
+++ codewave_activate.py
@@ -1,19 +1,32 @@
 import os.path
+import imp, sys
 import Npp
 # reload
 import codewave_npp.codewave.codewave
-reload(codewave_npp.codewave.codewave)
 import codewave_npp.npp_editor
 reload(codewave_npp.npp_editor)
 import codewave_npp.codewave.logger
-reload(codewave_npp.codewave.logger)
 import codewave_npp.codewave.storage
-reload(codewave_npp.codewave.storage)
 
 def consoleWriteFunct(txt):
 	Npp.console.write(txt)
 		
 debug = True
+
+
+if debug :
+	# reload
+	try :
+		for m in sys.modules.values() :
+			try :
+				if hasattr(m,'__file__') and "codewave" in m.__file__.lower() :
+					Npp.console.write("reload :" + m.__name__)
+					imp.reload(m)
+			except Exception as e:
+				Npp.console.write("reload failed :" + str(e))
+	except Exception as e:
+		Npp.console.write("reloads failed :" + str(e))
+		
 if debug or 'cw' not in vars() or cw is None :
 	codewave_npp.codewave.logger.WRITE_FUNCT = consoleWriteFunct
 	codewave_npp.codewave.logger.log('init codewave');
